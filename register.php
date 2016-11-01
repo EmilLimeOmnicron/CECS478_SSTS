74b7d64aca7b9e18446217b3960573a8cd0402ad
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2016-11-01 04:18:19 -0700

    Update register.php

--- register.php
+++ register.php
@@ -4,21 +4,24 @@ session_start();
 $username = strip_tags($_POST['username']); 
 $email = strip_tags($_POST['email']);
  $pass = strip_tags($_POST['password']);
- $hashAndSalt = password_hash($pass, PASSWORD_BCRYPT);
+//need to hash password for storing 
+$hashAndSalt = password_hash($pass, PASSWORD_BCRYPT);
+//sql statements to use
  $stmt1 = mysqli_prepare($conn, "insert into users(username, email, password) values (?,?,?)");
  $stmt2 = mysqli_prepare($conn, "SELECT * from users where email = ?");
  $stmt3 = mysqli_prepare($conn, "SELECT * from users where username = ?");
- mysqli_stmt_bind_param($stmt1, "sss", $username, $email, $hashAndSalt);
+//params used for the prepared statements
+mysqli_stmt_bind_param($stmt1, "sss", $username, $email, $hashAndSalt);
  mysqli_stmt_bind_param($stmt2, "s", $email); 
 mysqli_stmt_bind_param($stmt3, "s", $username);
+//executing the validations
  mysqli_stmt_execute($stmt2);
  mysqli_stmt_execute($stmt3);
  mysqli_stmt_store_result($stmt2);
  mysqli_stmt_store_result($stmt3);
  $row2 = mysqli_stmt_num_rows($stmt2);
  $row3 = mysqli_stmt_num_rows($stmt3);
- $response = array();
- $response["success"] = false;
+
  if($row2 > 1 and $row3 > 1) {
         echo 'username/email is not evailable';
 }
@@ -27,7 +30,8 @@ else{
     mysqli_stmt_execute($stmt1);
     mysqli_stmt_close($stmt1);
 }
+//clean up environment
 mysqli_stmt_close($stmt2);
  mysqli_stmt_close($stmt3);
- echo json_encode($response);
+
 ?>
