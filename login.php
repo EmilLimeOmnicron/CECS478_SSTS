6cd6be4be4ea0fc12eb5074008f5541c3090e4cd
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2016-11-03 23:50:00 -0700

    Update login.php
    updated final login from phase 2

--- login.php
+++ login.php
@@ -1,55 +1,138 @@
 <?php
 require_once 'dbconnect.php';
+require_once('vendor/autoload.php');
 use \Firebase\JWT\JWT;
+define('ALGORITHM','HS512');   // Algorithm used to signthe token, see
+define('SECRET_KEY','Your-Secret-key');
 
-define('SECRET_KEY','fill_secret'); 
-define('ALGORITHM','HS512');
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
 
-    $username = !empty($_POST['username']);
-    $pass = !empty($_POST['password']);
+echo "username '$username', password '$pass'\n";
 
-                // if there is no error below code run
-                $statement = $conn->prepare("select * from users where username = :username" );
-                $statement->execute(array(':username' => $username));
-                $row = $statement->fetchAll(PDO::FETCH_ASSOC);
-                $hashAndSalt = password_hash($password, PASSWORD_BCRYPT);
-                
-                if(count($row)>0 && password_verify($row[0]['password'],$hashAndSalt)) {
-                    $tokenId    = base64_encode(mcrypt_create_iv(32));
+$stmt = mysqli_stmt_init($conn);
+
+if(mysqli_stmt_prepare($stmt, "SELECT COUNT(username) AS num FROM users WHERE username = ?"))
+{
+  echo "PREPARE WORKS\n";
+}
+else
+{
+  echo "PREPARE DIDNT WORK\n";
+}
+
+if(mysqli_stmt_bind_param($stmt, "s", $username))
+{
+   echo "BIND WORKS\n";
+}
+else
+{
+   echo "BIND BROKE\n";
+}
+
+
+if(mysqli_stmt_execute($stmt))
+{
+  echo "EXECUTED\n";
+}
+else
+{
+  echo "NOT EXECUTED\n";
+}
+
+if(mysqli_stmt_store_result($stmt))
+{
+  echo "STORED\n";
+}
+else
+{
+  echo "NOT STORED\n";
+}
+
+if(mysqli_stmt_fetch ($stmt))
+{
+	  echo "RESULT FETCHED\n";
+
+	
+	}
+else
+{
+  echo "RESULT NOT FETCHED\n";
+}
+
+printf("Number of rows: %d.\n", mysqli_stmt_num_rows($stmt));
+
+echo "im right before the row num check";
+
+if(mysqli_stmt_num_rows($stmt) > 0)
+{
+  echo "GET PASS\n";
+
+  mysqli_stmt_free_result($stmt);
+  $getPass = mysqli_stmt_init($conn);
+echo "INIT\n";
+ if( mysqli_stmt_prepare($getPass, "SELECT password FROM users WHERE username = ?"))
+{echo "PREP\n";}
+ if(mysqli_stmt_bind_param($getPass, "s", $username))
+{echo "BIND\n";}
+  if(mysqli_stmt_execute($getPass))
+{echo "EXECUTE\n";}
+  if( $result = mysqli_stmt_get_result($getPass))
+{echo "getResult\n";}
+  printf("Number of rows: %d\n", $hashedPass);
+  if($hashedPass = mysqli_fetch_assoc($result)) {
+
+	    echo "FETCH\n";
+   }
+
+
+  echo "HASHED PASS ".$hashedPass['password']."\n";
+
+
+
+        if (password_verify($pass, $hashedPass['password']))
+                {
+echo "password valid"; 
+                $tokenId    = base64_encode(mcrypt_create_iv(32));
+
                     $issuedAt   = time();
                     $notBefore  = $issuedAt + 10;  //Adding 10 seconds
                     $expire     = $notBefore + 60; // Adding 60 seconds
                     $serverName = 'localhost'; /// set your domain name
+echo "this is the tken ID";
+echo $tokenID;
 
-
-                    /*
-                     * Create the token as an array
-                     */
-                    $data = [
+                   $data = [
                         'iat'  => $issuedAt,         // Issued at: time when the token was generated
                         'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
                         'iss'  => $serverName,       // Issuer
                         'nbf'  => $notBefore,        // Not before
                         'exp'  => $expire,           // Expire
-                        'data' => [                  // Data related to the logged user you can set your required data
-                                    'id'   => $row[0]['id'], // id from the users table
-                                     'name' => $row[0]['name'], //  name
-                             
+                        'data' => [                  // Data related to the logged user you can set your required data                                                                                                                                                         
+                                //    'id'   => ['id'], // id from the users table
+                                     'name' => $username, //  name 
                                   ]
                     ];
-
-           
+echo "this is the data";
+echo $username;         
+        echo $data;
                   $secretKey = base64_decode(SECRET_KEY);
                   /// Here we will transform this array into JWT:
+
+echo "this is the secret key";
+echo $secretKey;
+
                   $jwt = JWT::encode(
                             $data, //Data to be encoded in the JWT
-                            $secretKey // The signing key
-
-                           );
-                 $unencodedArray = ['jwt' => $jwt];
-                  echo json_encode($unencodedArray);
- } else {
-                  echo  "{'status' : 'error','msg':'Invalid email or password'}";
+                            SECRET_KEY, // The signing key
+                          ALGORITHM
+                                 );
+echo $jwt; 
+              // $unencodedArray = ['jwt' => $jwt];
+               //   echo json_encode($unencodedArray);
+                 } else {
+                  echo  "{'status' : 'error','msg':'Invalid username or password'}";
                   }
-   
+}
 ?>
+
