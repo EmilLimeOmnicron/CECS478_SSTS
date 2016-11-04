cbac28ce6a5990fe4309c5c1a5ff045bd714fd69
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2016-11-03 23:52:13 -0700

    Update dbconnect.php
    updated final databaseconnect from phase 2

--- dbconnect.php
+++ dbconnect.php
@@ -1,16 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "filler";
 
-try {
-$conn = new PDO("mysql:host=$servername;dbname=webdata", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Connected successfully";
+$conn = mysqli_connect("localhost", "dbusername", "dbpassword", "databaseName");
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
-catch(PDOException $e)
-{
-echo "Connection failed: " . $e->getMessage();
-}
-   
+echo 'passed first if block';
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
 ?>
