0404bdfe870de4dfebcc5e0b481ac919e4c192e1
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2016-11-06 18:50:37 -0800

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,10 @@
 require_once 'dbconnect.php';
 require_once('vendor/autoload.php');
 use \Firebase\JWT\JWT;
-define('ALGORITHM','HS512');   // Algorithm used to signthe token, see
-define('SECRET_KEY','Your-Secret-key');
+define('ALGORITHM','HS512');   
+define('SECRET_KEY','Your-Secret-key'); //placeholder key
 
+//Login uses username and password
     $username = $_POST['username'];
     $pass = $_POST['password'];
 
@@ -12,96 +13,58 @@ echo "username '$username', password '$pass'\n";
 
 $stmt = mysqli_stmt_init($conn);
 
-if(mysqli_stmt_prepare($stmt, "SELECT COUNT(username) AS num FROM users WHERE username = ?"))
-{
-  echo "PREPARE WORKS\n";
+//SQL statements for the correct login of the user.
+if(mysqli_stmt_prepare($stmt, "SELECT COUNT(username) AS num FROM users WHERE username = ?")){
 }
-else
-{
-  echo "PREPARE DIDNT WORK\n";
+else{
 }
 
-if(mysqli_stmt_bind_param($stmt, "s", $username))
-{
-   echo "BIND WORKS\n";
+if(mysqli_stmt_bind_param($stmt, "s", $username)){
 }
-else
-{
-   echo "BIND BROKE\n";
+else {
 }
 
-
-if(mysqli_stmt_execute($stmt))
-{
-  echo "EXECUTED\n";
+if(mysqli_stmt_execute($stmt)){
 }
-else
-{
-  echo "NOT EXECUTED\n";
+else{
 }
 
-if(mysqli_stmt_store_result($stmt))
-{
-  echo "STORED\n";
+if(mysqli_stmt_store_result($stmt)){
 }
-else
-{
-  echo "NOT STORED\n";
+else{
 }
 
-if(mysqli_stmt_fetch ($stmt))
-{
-	  echo "RESULT FETCHED\n";
-
-	
+if(mysqli_stmt_fetch ($stmt)){
 	}
-else
-{
-  echo "RESULT NOT FETCHED\n";
+else{
 }
 
-printf("Number of rows: %d.\n", mysqli_stmt_num_rows($stmt));
-
-echo "im right before the row num check";
-
-if(mysqli_stmt_num_rows($stmt) > 0)
-{
-  echo "GET PASS\n";
+if(mysqli_stmt_num_rows($stmt) > 0){
 
   mysqli_stmt_free_result($stmt);
   $getPass = mysqli_stmt_init($conn);
-echo "INIT\n";
- if( mysqli_stmt_prepare($getPass, "SELECT password FROM users WHERE username = ?"))
-{echo "PREP\n";}
- if(mysqli_stmt_bind_param($getPass, "s", $username))
-{echo "BIND\n";}
-  if(mysqli_stmt_execute($getPass))
-{echo "EXECUTE\n";}
-  if( $result = mysqli_stmt_get_result($getPass))
-{echo "getResult\n";}
-  printf("Number of rows: %d\n", $hashedPass);
+
+ if( mysqli_stmt_prepare($getPass, "SELECT password FROM users WHERE username = ?")){
+ }
+ if(mysqli_stmt_bind_param($getPass, "s", $username)){
+ }
+  if(mysqli_stmt_execute($getPass)){
+  }
+  if( $result = mysqli_stmt_get_result($getPass)){
+  }
+  
   if($hashedPass = mysqli_fetch_assoc($result)) {
-
-	    echo "FETCH\n";
    }
+	//Verify if password is correct
+        if (password_verify($pass, $hashedPass['password'])) {
 
-
-  echo "HASHED PASS ".$hashedPass['password']."\n";
-
-
-
-        if (password_verify($pass, $hashedPass['password']))
-                {
-echo "password valid"; 
                 $tokenId    = base64_encode(mcrypt_create_iv(32));
 
                     $issuedAt   = time();
                     $notBefore  = $issuedAt + 10;  //Adding 10 seconds
                     $expire     = $notBefore + 60; // Adding 60 seconds
                     $serverName = 'localhost'; /// set your domain name
-echo "this is the tken ID";
-echo $tokenID;
-
+                   //Bundle for token
                    $data = [
                         'iat'  => $issuedAt,         // Issued at: time when the token was generated
                         'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
@@ -113,23 +76,17 @@ echo $tokenID;
                                      'name' => $username, //  name 
                                   ]
                     ];
-echo "this is the data";
-echo $username;         
-        echo $data;
+
                   $secretKey = base64_decode(SECRET_KEY);
-                  /// Here we will transform this array into JWT:
-
-echo "this is the secret key";
-echo $secretKey;
-
+              
+                  //encode token
                   $jwt = JWT::encode(
                             $data, //Data to be encoded in the JWT
                             SECRET_KEY, // The signing key
                           ALGORITHM
                                  );
-echo $jwt; 
-              // $unencodedArray = ['jwt' => $jwt];
-               //   echo json_encode($unencodedArray);
+                 echo $jwt; 
+     
                  } else {
                   echo  "{'status' : 'error','msg':'Invalid username or password'}";
                   }
