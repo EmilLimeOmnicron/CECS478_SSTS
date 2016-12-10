8f2bdb271f79bee3a4bb1f1cd5dafc9d624ea4b7
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2016-12-09 18:32:39 -0800

    Update GetMessage.php

--- GetMessage.php
+++ GetMessage.php
@@ -5,56 +5,50 @@ require_once 'dbconnect.php';
 use \Firebase\JWT\JWT;
 
 define('ALGORITHM', 'HS512');
-define('SECRET_KEY','s25cLeE3205q4UwNG39ENX4lDGN63awS');
+define('SECRET_KEY','-');
+
 
 
   $headers = apache_request_headers();
   $token = $headers['token'];
-//read username from token
+
 try {
      $jwt = JWT::decode($token, SECRET_KEY, array(ALGORITHM));
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-//if we have a token
-if(isset($jwt)) {
-	echo "JWT DECODE\n";
-		}
+if(isset($jwt)) {echo "JWT DECODE\n";}
 
      $jwtArr = (array) $jwt;
 if(isset($jwtArr)) {echo "JWT ARRAY\n";}
 
+
      $username = $jwtArr['data']->name;
-//check if name exists from token
-if(isset($username)) {
-	echo "JWT DONE, user SET FROM TOKEN\n";
-		     }
+if(isset($username)) {echo "JWT DONE, user SET FROM TOKEN\n";}
 
 if(isset($username)) {
 echo "$username 's messages\n";
 
   $stmt = mysqli_stmt_init($conn);
 echo "INIT\n";
- if( mysqli_stmt_prepare($stmt, "SELECT * FROM messages WHERE receiver = ? OR sender = ?")){
-	 echo "PREP\n";
-}
- if(mysqli_stmt_bind_param($stmt, "ss", $username, $username)){
-	 echo "BIND\n";
-}
-  if(mysqli_stmt_execute($stmt)){
-	  echo "EXECUTE\n";
-}
-  if( $result = mysqli_stmt_get_result($stmt)) {
-	  echo "getResult\n";
-}
+ if( mysqli_stmt_prepare($stmt, "SELECT * FROM messages WHERE receiver = ? OR sender = ?"))
+{echo "PREP\n";}
+ if(mysqli_stmt_bind_param($stmt, "ss", $username, $username))
+{echo "BIND\n";}
+  if(mysqli_stmt_execute($stmt))
+{echo "EXECUTE\n";}
+  if( $result = mysqli_stmt_get_result($stmt))
+{echo "getResult\n";}
+
+
+
 
 $numMessages = mysqli_num_rows ($result);
 printf("Result set has %d rows.\n", $numMessages);
 
 
+
 $c = 0;
-	
-	//print all messages of current user.
          while($row = mysqli_fetch_assoc($result)) {
                 echo "".$row['message']."\n";
         }
@@ -65,4 +59,6 @@ else {
 }
 
 ?>
+
+
 
