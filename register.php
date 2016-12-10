a2a707569ef994903c5c94158dab9cbb914c8f46
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2016-12-09 18:37:35 -0800

    Update register.php

--- register.php
+++ register.php
@@ -1,14 +1,14 @@
+
 <?php
 session_start();
  require 'dbconnect.php';
+ ///$conn = mysqli_connect("localhost", "root", "Kappa123", "webdata");
 
-//Register using username, email, and password.
  $username =strip_tags($_POST['username']);
  $email =strip_tags($_POST['email']);
  $pass = strip_tags($_POST['password']);
  $hashAndSalt = password_hash($pass, PASSWORD_BCRYPT);
 
-//Prepared Statements
  $stmt1 = mysqli_prepare($conn, "insert into users(username, email, password) values (?,?,?)");
  $stmt2 = mysqli_prepare($conn, "SELECT * from users where email = ?");
  $stmt3 = mysqli_prepare($conn, "SELECT * from users where username = ?");
@@ -16,10 +16,11 @@ session_start();
  mysqli_stmt_bind_param($stmt2, "s", $email);
  mysqli_stmt_bind_param($stmt3, "s", $username);
 
- echo "beginInfo '$username', '$email', '$pass', '$hashAndSalt'  endInfo";
+// echo "beginInfo '$username', '$email', '$pass', '$hashAndSalt'  endInfo";
 
-
- mysqli_stmt_execute($stmt2);                                                                                                                                                                                                                                                  
+ ///http://php.net/manual/en/mysqli-stmt.execute.php
+ ///I think we need to use fetch() to store results before making another query
+ mysqli_stmt_execute($stmt2);
  mysqli_stmt_store_result($stmt2);
  $row2 = mysqli_stmt_num_rows($stmt2);
 
@@ -27,20 +28,20 @@ session_start();
  mysqli_stmt_store_result($stmt3);
  $row3 = mysqli_stmt_num_rows($stmt3);
 
- printf("Number of emails: %d.\n", mysqli_stmt_num_rows($stmt2));
- printf("Number of usernames: %d.\n", mysqli_stmt_num_rows($stmt3));
+// printf("Number of emails: %d.\n", mysqli_stmt_num_rows($stmt2));
+// printf("Number of usernames: %d.\n", mysqli_stmt_num_rows($stmt3));
 
+ ///Changed and to or
  ///stop if username OR email are taken
  if($row2 != 0 or $row3  != 0) {
-        echo 'username/email is not available';
+        echo 'username/email is not evailable';
  }
  else{
     echo "Thanks for registering";
     mysqli_stmt_execute($stmt1);
-    printf("rows inserted: %d\n", mysqli_stmt_affected_rows($stmt1));
+//    printf("rows inserted: %d\n", mysqli_stmt_affected_rows($stmt1));
     mysqli_stmt_close($stmt1);
  }
-//Clean up environment
  mysqli_stmt_close($stmt2);
  mysqli_stmt_close($stmt3);
 
